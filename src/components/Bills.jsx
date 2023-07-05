adb513edc10ea8705da0c98f31817e1e3e454e10
Mumbi30! <github email address>
2023-07-05 08:56:57 +0300

    Assignment in Progress

--- src/components/Bills.jsx
+++ src/components/Bills.jsx
@@ -1,6 +1,30 @@
-function Bills(){
-return(
-    <div>hello world</div>)
-}
+import React, { useState, useEffect } from "react";
+import { Card, Button, Header, Divider } from "react-bootstrap";
+import Bill from "./Bill";
+
+const Bills = () => {
+  const [bills, setBills] = useState([]);
+
+  useEffect(() => {
+    // Get the bills from the server
+    fetch("/api/bills")
+      .then((res) => res.json())
+      .then((bills) => setBills(bills));
+  }, []);
+
+  return (
+    <div>
+      <Header>
+        <h1>Bills</h1>
+      </Header>
+      <Divider />
+      {bills.map((bill) => (
+        <Card key={bill.id}>
+          <Bill bill={bill} />
+        </Card>
+      ))}
+    </div>
+  );
+};
 
 export default Bills;
