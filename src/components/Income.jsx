7b7ab523a5666250922d7d07d6abc84e02dede52
henry <henry.waweru@student.moringaschool.com>
2023-07-05 21:48:00 +0300

    income

--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -1,6 +1,41 @@
-function Income(){
-return(
-    <div>hello world</div>)
+import React, { useEffect, useState } from 'react';
+
+function Income() {
+  const [incomeData, setIncomeData] = useState([]);
+
+  useEffect(() => {
+    // Fetch income data from the backend
+    fetch('/api/income') // Replace with your actual API endpoint
+      .then(response => response.json())
+      .then(data => setIncomeData(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Income</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Source</th>
+            <th>Description</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {incomeData.map((income) => (
+            <tr key={income.id}>
+              <td>{income.date}</td>
+              <td>{income.source}</td>
+              <td>{income.description}</td>
+              <td>{income.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Income;
